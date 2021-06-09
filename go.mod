70441cf96ee9a658c6a2f24728d791d7f17a2794
Bartek Pacia <barpac02@gmail.com>
2021-06-10 00:27:53 +0200

    works hehe

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/bartekpacia/dondu
 
 go 1.16
+
+require github.com/joho/godotenv v1.3.0
--- main.go
+++ main.go
@@ -2,22 +2,33 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
 	"net/url"
+	"os"
 	"strconv"
 	"strings"
+
+	"github.com/joho/godotenv"
+)
+
+const (
+	BaseURL    = "https://pracownia.knei.pl"
+	LoginURL   = BaseURL + "/" + "?m=wyloguj" // login subpage address
+	ActionsURL = BaseURL + "/" + "akcje.php"  // actions subpage address
 )
 
 var (
-	ApiUrl = "https://pracownia.knei.pl/akcje.php"
+	username string
+	password string
 )
 
 var (
 	socketNumber int
 	socketState  int
 	minutes      int
-	changeState  int
 )
 
 func init() {
@@ -26,27 +37,85 @@ func init() {
 
 	flag.IntVar(&socketNumber, "socket", 0, "number of the socket to manipulate")
 	flag.IntVar(&socketState, "state", 0, "state to set on socket ")
-	flag.IntVar(&minutes, "minutes", 0, "time")
+	flag.IntVar(&minutes, "minutes", 5, "time")
+	flag.Parse()
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("failed to load .env file")
+	}
+
+	username = os.Getenv("DONDU_USERNAME")
+	password = os.Getenv("DONDU_PASSWORD")
 }
 
 func main() {
-	/*
-		gniazdo_nr: 5
-		gniazdo_stan: 0
-		minuty:
-		rozdzielnia_zmien_stan: 1
-	*/
+	var err error
+	http.DefaultClient.Jar, err = cookiejar.New(nil)
+	if err != nil {
+		log.Fatalln("failed to create cookie jar")
+	}
 
+	err = auth()
+	if err != nil {
+		log.Fatalln("failed to authencticate:", err)
+	}
+
+	fmt.Println("---")
+	base, err := url.Parse(BaseURL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, cookie := range http.DefaultClient.Jar.Cookies(base) {
+		log.Println("url cookie:", cookie)
+	}
+	fmt.Println("---")
+
+	err = update()
+	if err != nil {
+		log.Fatalln("failed to update the switchboard:", err)
+	}
+}
+
+// auth performs user authentication (sets the php session id cookie).
+func auth() error {
+	data := url.Values{}
+	data.Set("login", username)
+	data.Set("haslo", password)
+
+	log.Printf("body: %s\n", data.Encode())
+	res, err := http.Post(LoginURL, "application/x-www-form-urlencoded", strings.NewReader(data.Encode()))
+	if err != nil {
+		return fmt.Errorf("failed to make HTTP POST login request: %v", err)
+	}
+
+	log.Println("auth status:", res.Status)
+
+	for _, cookie := range res.Cookies() {
+		log.Println("cookie:", cookie)
+	}
+
+	return nil
+}
+
+// update modifies state of single socket on the switchboard.
+func update() error {
 	data := url.Values{}
 	data.Set("gniazdo_nr", strconv.Itoa(socketNumber))
 	data.Set("gniazdo_stan", strconv.Itoa(socketState))
+	data.Set("minuty", strconv.Itoa(socketState))
 	data.Set("rozdzielnia_zmien_stan", strconv.Itoa(1))
 
-	res, err := http.Post(ApiUrl, "application/x-www-form-urlencoded", strings.NewReader(data.Encode()))
+	res, err := http.Post(ActionsURL, "application/x-www-form-urlencoded", strings.NewReader(data.Encode()))
 	if err != nil {
 		log.Fatalf("failed to make HTTP POST request: %v\n", err)
 	}
 
+	for _, cookie := range res.Cookies() {
+		log.Println("cookie:", cookie)
+	}
+
 	log.Printf("res: %v\n", res)
+	return nil
 }
