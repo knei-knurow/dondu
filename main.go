fa90fd17b03cf98b0e12efc6c7dd72ff08254789
Bartek Pacia <barpac02@gmail.com>
2021-06-10 01:12:46 +0200

    remove redundant logging

--- main.go
+++ main.go
@@ -37,7 +37,7 @@ func init() {
 
 	flag.IntVar(&socketNumber, "socket", 0, "number of the socket to manipulate")
 	flag.IntVar(&socketState, "state", 0, "state to set on socket ")
-	flag.IntVar(&minutes, "minutes", 5, "time")
+	flag.IntVar(&minutes, "minutes", 30, "time")
 	flag.Parse()
 
 	err := godotenv.Load()
@@ -61,40 +61,24 @@ func main() {
 		log.Fatalln("failed to authencticate:", err)
 	}
 
-	fmt.Println("---")
-	base, err := url.Parse(BaseURL)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	for _, cookie := range http.DefaultClient.Jar.Cookies(base) {
-		log.Println("url cookie:", cookie)
-	}
-	fmt.Println("---")
-
 	err = update()
 	if err != nil {
 		log.Fatalln("failed to update the switchboard:", err)
 	}
 }
 
-// auth performs user authentication (sets the php session id cookie).
+// auth performs user authentication (sets the PHPSESSID cookie).
 func auth() error {
 	data := url.Values{}
 	data.Set("login", username)
 	data.Set("haslo", password)
 
-	log.Printf("body: %s\n", data.Encode())
 	res, err := http.Post(LoginURL, "application/x-www-form-urlencoded", strings.NewReader(data.Encode()))
 	if err != nil {
 		return fmt.Errorf("failed to make HTTP POST login request: %v", err)
 	}
 
 	log.Println("auth status:", res.Status)
-
-	for _, cookie := range res.Cookies() {
-		log.Println("cookie:", cookie)
-	}
 
 	return nil
 }
@@ -107,15 +91,10 @@ func update() error {
 	data.Set("minuty", strconv.Itoa(socketState))
 	data.Set("rozdzielnia_zmien_stan", strconv.Itoa(1))
 
-	res, err := http.Post(ActionsURL, "application/x-www-form-urlencoded", strings.NewReader(data.Encode()))
+	_, err := http.Post(ActionsURL, "application/x-www-form-urlencoded", strings.NewReader(data.Encode()))
 	if err != nil {
-		log.Fatalf("failed to make HTTP POST request: %v\n", err)
+		return fmt.Errorf("failed to make HTTP POST request: %v", err)
 	}
 
-	for _, cookie := range res.Cookies() {
-		log.Println("cookie:", cookie)
-	}
-
-	log.Printf("res: %v\n", res)
 	return nil
 }
